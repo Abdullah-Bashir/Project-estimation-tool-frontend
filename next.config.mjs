ea2cbf1a346e05784af5e92346eee5a76387b3d2
Abdullah-Bashir <killersaeed007@gmail.com>
2025-04-22 16:13:03 +0500

    sdsd

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-// next.config.js
-module.exports = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                crypto: require.resolve('crypto-browserify'),
-            };
-        }
-        return config;
-    },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
