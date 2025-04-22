b0d3d28da608774f0b30afdad8c18f9f93f10092
Abdullah-Bashir <killersaeed007@gmail.com>
2025-04-22 16:35:15 +0500

    sdsd

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            crypto: require.resolve('crypto-browserify'),
-            stream: require.resolve('stream-browserify'),
-            buffer: require.resolve('buffer'),
-        };
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
